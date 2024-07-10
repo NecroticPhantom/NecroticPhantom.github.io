827b73f46cb6397bce921892ea1be01a149dee87
NecroticPhantom <167430021+NecroticPhantom@users.noreply.github.com>
2024-07-10 15:31:15 +0100

    update

---
+++ portfolio_files/portfolio_functions.js
@@ -0,0 +1,4 @@
+function portfolioTestDiv1Function() {
+    //WIP (will expand 'nothing' into an img + text of a project when hovered over (will be mutiple of these for different projects))
+}
+element.getElementById("portfolioTestDiv1").addEventListener("onmouseover", portfolioTestDiv1Function);
