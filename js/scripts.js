a183374fdb815dd951f663030c394b52f870d02b
NecroticPhantom <167430021+NecroticPhantom@users.noreply.github.com>
2024-07-13 11:23:57 +0100

    end of coding session (added lightmode/darkmode button functionality)

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+var lightModeButton = document.getElementById("lightModeButton");
+button.addEventListener("click", lightMode);
+function lightMode() {
+    const body = document.getElementsByTagName("body");
+    body.style.backgroundColor = "white";
+    const pageLinks = document.getElementsByClassName("pageLinks");
+    pageLinks.style.backgroundColor = "mediumblue";
+    const currentPageLink = document.getElementsByClassName("currentPageLink");
+    currentPageLink.style.backgroundColor = "white";
+    const pageLinksText = document.getElementsByClassName("pageLinksText");
+    pageLinksText.style.backgroundColor = "white";
+    const textContainer = document.getElementsByClassName("textContainer");
+    textContainer.style.backgroundColor = "white";
+    const text = document.getElementsByClassName("text");
+    text.style.color = "black";
+};
+var darkModeButton = document.getElementById("darkModeButton");
+button.addEventListener("click", darkMode);
+function darkMode() {
+    const body = document.getElementsByTagName("body");
+    body.style.backgroundColor = "black";
+    const pageLinks = document.getElementsByClassName("pageLinks");
+    pageLinks.style.backgroundColor = "lime";
+    const currentPageLink = document.getElementsByClassName("currentPageLink");
+    currentPageLink.style.backgroundColor = "black";
+    const pageLinksText = document.getElementsByClassName("pageLinksText");
+    pageLinksText.style.backgroundColor = "black";
+    const textContainer = document.getElementsByClassName("textContainer");
+    textContainer.style.backgroundColor = "black";
+    const text = document.getElementsByClassName("text");
+    text.style.color = "white";
+};
