b98d325d502d18e3a773fa1ad59206f91d55125c
NecroticPhantom <167430021+NecroticPhantom@users.noreply.github.com>
2024-07-13 13:07:55 +0100

    small css changes

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ lmb.addEventListener("click", lightMode);
 function lightMode() {
     lmb.id = "darkModeButton";
     lmb.style.borderColor = "mediumblue";
-    dmb.style.backgroundColor = "black";
+    lmb.style.backgroundColor = "black";
     document.getElementById("lightModeButtonText").innerText = "Dark Mode";
     document.getElementsByTagName("body").style.backgroundColor = "white";
     document.getElementsByClassName("pageLinks").style.backgroundColor = "mediumblue";
@@ -16,6 +16,8 @@ var dmb = document.getElementById("darkModeButton");
 dmb.addEventListener("click", darkMode);
 function darkMode() {
     dmb.id = "lightModeButton";
+    dmb.style.borderColor = "lime";
+    dmb.style.backgroundColor = "white";
     document.getElementById("lightModeButtonText").innerText = "Light Mode";
     document.getElementsByTagName("body").style.backgroundColor = "black";
     document.getElementsByClassName("pageLinks").style.backgroundColor = "lime";
