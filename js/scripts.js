6d6d0e4a790e3415f7e446329e065bd340662832
NecroticPhantom <167430021+NecroticPhantom@users.noreply.github.com>
2024-07-15 10:31:40 +0100

    fixed colour change (chooseColour function)

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 function chooseColour() {
     var chosenColour = window.prompt("Type the javascript colour you would like to be used in the colour scheme:");
     var chosenColour = chosenColour.toLowerCase();
-    document.getElementsByClassName("pageLinks").style.backgroundColor = chosenColour;
+    var pageLinkElements = document.getElementsByClassName("pageLinks");
+    for (var i = 0; i < elements.length; i++) {
+    elements[i].style.backgroundColor = chosenColour;
+    };
 };
 function chooseMode() {
     var chosenMode = window.prompt("Would you like to use light mode or dark mode (Type: 'light' or 'dark')?");
@@ -13,19 +16,19 @@ function chooseMode() {
         darkMode()
     };
 };
-function lightMode() {
+/*function lightMode() {
     document.getElementsByTagName("body").style.backgroundColor = "white";
     document.getElementsByClassName("pageLinks").style.backgroundColor = "mediumblue";
     document.getElementsByClassName("currentPageLink").style.backgroundColor = "white";
     document.getElementsByClassName("pageLinksText").style.backgroundColor = "white";
     document.getElementsByClassName("textContainer").style.backgroundColor = "white";
     document.getElementsByClassName("text").style.color = "black";
-};
-function darkMode() {
+}; */
+/*function darkMode() {
     document.getElementsByTagName("body").style.backgroundColor = "black";
     document.getElementsByClassName("pageLinks").style.backgroundColor = "lime";
     document.getElementsByClassName("currentPageLink").style.backgroundColor = "black";
     document.getElementsByClassName("pageLinksText").style.backgroundColor = "black";
     document.getElementsByClassName("textContainer").style.backgroundColor = "black";
     document.getElementsByClassName("text").style.color = "white";
-};
+}; */
