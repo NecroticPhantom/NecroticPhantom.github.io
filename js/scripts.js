377daa4071bc3100339b5aef128858a11351edf7
NecroticPhantom <167430021+NecroticPhantom@users.noreply.github.com>
2024-07-15 11:40:41 +0100

    fixed js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function customiseSiteNow() {
-    var customisingSiteNow = window.prompt("Would you like to customise the website now?")
+    var customisingSiteNow = window.prompt("Would you like to customise the website now?");
     var customisingSiteNow = customisteSiteNow.toLowerCase();
     if (customisingSiteNow == "no") {
         customisingSiteNow = False;
