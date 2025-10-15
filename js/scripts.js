52ea1260779c25b25c5c5bfce75e3c886c2b4ff5
NecroticPhantom <167430021+NecroticPhantom@users.noreply.github.com>
2025-10-15 11:35:50 +0100

    Added test page + interactive page link hover (test)

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-// WIP
+document.getElementByClass("pageLinks").onmouseover = function() {pageLinkHover()};
+document.getElementByClass("pageLinks").onmouseout = function() {pageLinkStopHover()};
+
+function pageLinkHover() {
+  document.getElementByClass("pageLinks").style.backgroundColor = "cornflowerblue";
+}
+
+function pageLinkStopHover() {
+  document.getElementById("pageLinks").style.backgroundColor = "blue";
+}
