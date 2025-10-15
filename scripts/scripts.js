b49e0c7eedabcbcc840c838f66a0905054545851
NecroticPhantom <167430021+NecroticPhantom@users.noreply.github.com>
2025-10-15 12:12:44 +0100

    JS fix

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,16 @@
-document.getElementByClass("pageLinks").onmouseover = function() {pageLinkHover()};
-document.getElementByClass("pageLinks").onmouseout = function() {pageLinkStopHover()};
+window.addEventListener("load", () => {
+  document.getElementById("homeLink").addEventListener("mouseover", pageLinkHover("homeLink"))
+  document.getElementById("pathfindingLink").addEventListener("mouseout", pageLinkHover("pathfindingLink"))
+  document.getElementById("testPageLink").addEventListener("mouseout", pageLinkHover("testPageLink"))
+  document.getElementById("homeLink").addEventListener("mouseover", pageLinkStopHover("homeLink"))
+  document.getElementById("pathfindingLink").addEventListener("mouseout", pageLinkStopHover("pathfindingLink"))
+  document.getElementById("testPageLink").addEventListener("mouseout", pageLinkStopHover("testPageLink"))
+});
 
-function pageLinkHover() {
-  document.getElementByClass("pageLinks").style.backgroundColor = "cornflowerblue";
+function pageLinkHover(elementID) {
+  document.getElementById(elementID).style.backgroundColor = "cornflowerblue";
 }
 
 function pageLinkStopHover() {
-  document.getElementById("pageLinks").style.backgroundColor = "blue";
+  document.getElementById(elementID).style.backgroundColor = "blue";
 }
